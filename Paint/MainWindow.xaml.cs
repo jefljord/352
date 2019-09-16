c21c77fee66e50d6a4cc0ca89bb93e48f8af655e
jeff jordan <jeffjordan33@outlook.com>
2019-09-16 13:46:53 -0500

    fin

--- Paint/MainWindow.xaml.cs
+++ Paint/MainWindow.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace Paint
 {
@@ -19,7 +21,7 @@ namespace Paint
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {       
         public MainWindow()
         {
             InitializeComponent();
@@ -29,5 +31,31 @@ namespace Paint
         {
             Inkcanvas.Strokes.Clear();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Inkcanvas.DefaultDrawingAttributes.Color = Color.FromRgb(200, 54, 150);
+           
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+
+            Inkcanvas.DefaultDrawingAttributes.Color = Color.FromRgb(200, 54, 150);
+
+        }
+
+        private void Size_Click(object sender, RoutedEventArgs e)
+        {
+            Inkcanvas.DefaultDrawingAttributes.Width = 5;
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Inkcanvas.DefaultDrawingAttributes.Color = Color.FromRgb(0, 0, 0);
+            Inkcanvas.DefaultDrawingAttributes.Width = 2;
+        }
     }
+    
 }
