dc6399983a7512901a0610dc979fd3201d4e8fbc
Jeffrey Jordan <jefljord@ut.utm.edu>
2019-10-03 14:39:40 -0500

    AS

---
+++ Paint/HelloFromMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Paint
+{
+    class HelloFromMain
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Paint.App app = new Paint.App();
+            app.InitializeComponent();
+            
+            MessageBoxResult hello = MessageBox.Show("Hello From Main");
+
+            app.Run();
+        }
+    }
+}
---
+++ Paint/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Paint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Inkcanvas.Strokes.Clear();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Inkcanvas.DefaultDrawingAttributes.Color = Color.FromRgb(200, 54, 150);
+           
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+
+            Inkcanvas.DefaultDrawingAttributes.Color = Color.FromRgb(200, 54, 150);
+
+        }
+
+        private void Size_Click(object sender, RoutedEventArgs e)
+        {
+            Inkcanvas.DefaultDrawingAttributes.Width = 5;
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Inkcanvas.DefaultDrawingAttributes.Color = Color.FromRgb(0, 0, 0);
+            Inkcanvas.DefaultDrawingAttributes.Width = 2;
+        }
+    }
+    
+}
