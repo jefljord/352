14ad6046bedb5a6dd5ef42507da3daf9fda8f6da
Jeffrey Jordan <jefljord@ut.utm.edu>
2019-10-03 15:52:16 -0500

    sdf

--- Paint/App.xaml.cs
+++ Paint/App.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Author: Jeffrey Jordan
+ * Date: 10/3/19
+ * File: App.xaml.cs
+ * Description: The file where logic ineraction takes place.
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
